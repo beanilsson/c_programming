d0e1af94b51af20b730330ac1d8f4f2dbdb02c91
Bea <beanilsson@users.noreply.github.com>
2018-03-26 11:12:00 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 sudo apt-get install -y build-essential
 sudo apt-get upgrade -y
 sudo apt-get install flex bison -y
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main"
+sudo apt-get update
+sudo apt-get install -y clang-5.0
